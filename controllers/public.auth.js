27c45ac406b6050ec7dbbf95f74725ecbd46d70c
Yazdun <yazduntube@gmail.com>
2022-03-04 09:31:10 +0330

    authentication : completed

--- controllers/public.auth.js
+++ controllers/public.auth.js
@@ -1,6 +1,6 @@
 const { User } = require('../models')
 const { StatusCodes } = require('http-status-codes')
-const { BadRequestError, NotFoundError } = require('../errors')
+const { BadRequestError, UnauthenticatedError } = require('../errors')
 
 const join = async (req, res) => {
   const usernameExists = await User.findOne({ username: req.body.username })
@@ -11,11 +11,28 @@ const join = async (req, res) => {
   const token = user.createJWT()
   res
     .status(StatusCodes.CREATED)
-    .json({ user: { username: user.username }, token })
+    .json({ user: { username: user.username, userId: user._id }, token })
 }
 
 const login = async (req, res) => {
-  res.end('login user')
+  const { username, password } = req.body
+
+  if (!username || !password)
+    throw new BadRequestError('username and password are required')
+
+  const user = await User.findOne({ username })
+  if (!user)
+    throw new UnauthenticatedError('username or password is not correct')
+
+  const isPasswordCorrect = await user.comparePassword(password)
+  if (!isPasswordCorrect)
+    throw new UnauthenticatedError('username or password is not correct')
+
+  const token = user.createJWT()
+  res.status(StatusCodes.OK).json({
+    user: { username: user.username, userId: user._id },
+    token,
+  })
 }
 
 module.exports = {
--- models/User.js
+++ models/User.js
@@ -43,4 +43,9 @@ UserSchema.methods.createJWT = function () {
   )
 }
 
+UserSchema.methods.comparePassword = async function (candidatePassword) {
+  const isMatch = await bcrypt.compare(candidatePassword, this.password)
+  return isMatch
+}
+
 module.exports = mongoose.model('User', UserSchema)
